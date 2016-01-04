e9fa3b36c1c65f20cbc67b271fb6e4a944c8aaa5
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2016-01-04 10:11:56 +0000

    Added pie chart control

---
+++ include/wx/charts/wxpiechartctrl.h
@@ -0,0 +1,92 @@
+/*
+	Copyright (c) 2016 Xavier Leclercq
+
+	Permission is hereby granted, free of charge, to any person obtaining a
+	copy of this software and associated documentation files (the "Software"),
+	to deal in the Software without restriction, including without limitation
+	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+	and/or sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+	IN THE SOFTWARE.
+*/
+
+#ifndef _WX_CHARTS_WXPIECHARTCTRL_H_
+#define _WX_CHARTS_WXPIECHARTCTRL_H_
+
+#include "wxchartarc.h"
+#include <wx/control.h>
+#include <vector>
+#include <memory>
+
+class wxPieChartOptions
+{
+public:
+	wxPieChartOptions();
+
+	unsigned int SegmentStrokeWidth() const;
+
+private:
+	// The width of each segment stroke, this will increase the 
+	// space between the segments themselves.
+	unsigned int m_segmentStrokeWidth;
+};
+
+class wxPieChartCtrl : public wxControl
+{
+public:
+	struct Segment
+	{
+		Segment(double value, const wxColor &color);
+
+		double value;
+		wxColor color;
+	};
+
+public:
+	wxPieChartCtrl(wxWindow *parent, wxWindowID id, const wxPoint &pos = wxDefaultPosition,
+		const wxSize &size = wxDefaultSize, long style = 0);
+
+	void AddData(const Segment &segment);
+	void AddData(const Segment &segment, size_t index);
+	void AddData(const Segment &segment, size_t index, bool silent);
+
+private:
+	double CalculateCircumference(double value);
+	void OnAddOrRemoveData();
+
+	void OnPaint(wxPaintEvent &evt);
+	void OnSize(wxSizeEvent& evt);
+
+private:
+	struct SegmentArc : public wxChartArc
+	{
+		typedef std::shared_ptr<SegmentArc> ptr;
+
+		SegmentArc(const Segment &segment, double x, double y, 
+			double startAngle, double endAngle, double outerRadius, 
+			unsigned int strokeWidth);
+
+		void Resize(const wxSize &size, const wxPieChartOptions& options);
+
+		double value;
+	};
+
+private:
+	wxPieChartOptions m_options;
+	std::vector<SegmentArc::ptr> m_segments;
+	double m_total;
+
+	DECLARE_EVENT_TABLE();
+};
+
+#endif
---
+++ src/wxpiechartctrl.cpp
@@ -0,0 +1,156 @@
+/*
+	Copyright (c) 2016 Xavier Leclercq
+
+	Permission is hereby granted, free of charge, to any person obtaining a
+	copy of this software and associated documentation files (the "Software"),
+	to deal in the Software without restriction, including without limitation
+	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+	and/or sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+	IN THE SOFTWARE.
+*/
+
+#include "wxpiechartctrl.h"
+#include <wx/dcbuffer.h>
+#include <wx/graphics.h>
+
+wxPieChartOptions::wxPieChartOptions()
+	: m_segmentStrokeWidth(2)
+{
+}
+
+unsigned int wxPieChartOptions::SegmentStrokeWidth() const
+{
+	return m_segmentStrokeWidth;
+}
+
+wxPieChartCtrl::Segment::Segment(double value,
+								 const wxColor &color)
+	: value(value), color(color)
+{
+}
+
+wxPieChartCtrl::SegmentArc::SegmentArc(const Segment &segment, 
+									   double x, 
+									   double y,
+									   double startAngle, 
+									   double endAngle, 
+									   double outerRadius,
+									   unsigned int strokeWidth)
+	: wxChartArc(x, y, startAngle, endAngle, outerRadius, 0, strokeWidth, segment.color),
+	value(segment.value)
+{
+}
+
+void wxPieChartCtrl::SegmentArc::Resize(const wxSize &size, 
+										const wxPieChartOptions& options)
+{
+	double x = (size.GetX() / 2) - 2;
+	double y = (size.GetY() / 2) - 2;
+	double outerRadius = ((x < y) ? x : y) - (StrokeWidth() / 2);
+	
+	setCenter(x, y);
+	setRadiuses(outerRadius, 0);
+}
+
+wxPieChartCtrl::wxPieChartCtrl(wxWindow *parent,
+							   wxWindowID id,
+							   const wxPoint &pos,
+							   const wxSize &size, 
+							   long style)
+	: wxControl(parent, id, pos, size, style), m_total(0)
+{
+	SetBackgroundStyle(wxBG_STYLE_PAINT);
+	SetBackgroundColour(*wxWHITE);
+}
+
+void wxPieChartCtrl::AddData(const Segment &segment)
+{
+	AddData(segment, m_segments.size());
+}
+
+void wxPieChartCtrl::AddData(const Segment &segment, size_t index)
+{
+	AddData(segment, index, false);
+}
+
+void wxPieChartCtrl::AddData(const Segment &segment, size_t index, bool silent)
+{
+	m_total += segment.value;
+
+	double x = (GetSize().GetX() / 2) - 2;
+	double y = (GetSize().GetY() / 2) - 2;
+	double outerRadius = ((x < y) ? x : y) - (m_options.SegmentStrokeWidth() / 2);
+	
+	SegmentArc::ptr newSegment = std::make_shared<SegmentArc>(segment,
+		x, y, 0, 0, outerRadius, m_options.SegmentStrokeWidth());
+	m_segments.insert(m_segments.begin() + index, newSegment);
+	if (!silent){
+		OnAddOrRemoveData();
+	}
+}
+
+double wxPieChartCtrl::CalculateCircumference(double value)
+{
+	if (m_total > 0) 
+	{
+		return (value * 2 * M_PI / m_total);
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+void wxPieChartCtrl::OnAddOrRemoveData()
+{
+}
+
+void wxPieChartCtrl::OnPaint(wxPaintEvent &evt)
+{
+	wxAutoBufferedPaintDC dc(this);
+	
+	dc.Clear();
+
+	wxGraphicsContext* gc = wxGraphicsContext::Create(dc);
+	if (gc)
+	{
+		double startAngle = 0.0;
+		for (size_t i = 0; i < m_segments.size(); ++i)
+		{
+			SegmentArc& currentSegment = *m_segments[i];
+			
+			double endAngle = startAngle + CalculateCircumference(currentSegment.value);
+			currentSegment.setAngles(startAngle, endAngle);
+			startAngle = endAngle;
+
+			currentSegment.draw(*gc);
+		}
+
+		delete gc;
+	}
+}
+
+void wxPieChartCtrl::OnSize(wxSizeEvent& evt)
+{
+	for (size_t i = 0; i < m_segments.size(); ++i)
+	{
+		m_segments[i]->Resize(evt.GetSize(),m_options);
+	}
+	Refresh();
+}
+
+BEGIN_EVENT_TABLE(wxPieChartCtrl, wxControl)
+	EVT_PAINT(wxPieChartCtrl::OnPaint)
+	EVT_SIZE(wxPieChartCtrl::OnSize)
+END_EVENT_TABLE()
