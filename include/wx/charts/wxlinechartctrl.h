49c488cc1fc4d8238c43bd2b81ae57d4c7edc392
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2016-01-21 10:17:21 +0000

    Added documentation

--- include/wx/charts/wxlinechartctrl.h
+++ include/wx/charts/wxlinechartctrl.h
@@ -76,9 +76,23 @@ private:
 	wxVector<wxLineChartDataset::ptr> m_datasets;
 };
 
+/// A control that displays a line chart.
 class wxLineChartCtrl : public wxChart
 {
 public:
+	/// Constructs a wxLineChartCtrl control.
+	/// @param parent Pointer to a parent window.
+	/// @param id Control identifier. If wxID_ANY, will automatically
+	/// create an identifier.
+	/// @param data The data that will be used to initialize the chart.
+	/// @param pos Control position. wxDefaultPosition indicates that 
+	/// wxWidgets should generate a default position for the control.
+	/// @param size Control size. wxDefaultSize indicates that wxWidgets
+	/// should generate a default size for the window. If no suitable 
+	/// size can  be found, the window will be sized to 20x20 pixels 
+	/// so that the window is visible but obviously not correctly sized.
+	/// @param style Control style. For generic window styles, please 
+	/// see wxWindow.
 	wxLineChartCtrl(wxWindow *parent, wxWindowID id, const wxLineChartData &data,
 		const wxPoint &pos = wxDefaultPosition, const wxSize &size = wxDefaultSize, 
 		long style = 0);
