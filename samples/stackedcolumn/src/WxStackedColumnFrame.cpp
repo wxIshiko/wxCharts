da6861c6de1f8766d364305b25b7066976e5c53f
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2016-02-11 21:25:16 +0000

    Updates

---
+++ samples/stackedcolumn/src/WxStackedColumnFrame.cpp
@@ -0,0 +1,87 @@
+/*
+	Copyright (c) 2016 Xavier Leclercq
+
+	Permission is hereby granted, free of charge, to any person obtaining a
+	copy of this software and associated documentation files (the "Software"),
+	to deal in the Software without restriction, including without limitation
+	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+	and/or sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+	IN THE SOFTWARE.
+*/
+
+#include "WxStackedColumnFrame.h"
+#include <wx/panel.h>
+#include <wx/sizer.h>
+#include <wx/charts/wxcharts.h>
+
+WxStackedColumnFrame::WxStackedColumnFrame(const wxString& title)
+	: wxFrame(NULL, wxID_ANY, title)
+{
+	// Create a top-level panel to hold all the contents of the frame
+	wxPanel* panel = new wxPanel(this, wxID_ANY);
+
+	// Create the data for the stacked bar chart widget
+	wxVector<wxString> labels;
+	labels.push_back("January");
+	labels.push_back("February");
+	labels.push_back("March");
+	labels.push_back("April");
+	labels.push_back("May");
+	labels.push_back("June");
+	labels.push_back("July");
+	wxStackedColumnChartData chartData(labels);
+
+	// Add the first dataset
+	wxVector<wxDouble> points1;
+	points1.push_back(3);
+	points1.push_back(2.5);
+	points1.push_back(1.2);
+	points1.push_back(3);
+	points1.push_back(4);
+	points1.push_back(5);
+	points1.push_back(1);
+	wxStackedColumnChartDataset::ptr dataset1(new wxStackedColumnChartDataset(
+		wxColor(220, 220, 220, 0x7F),
+		wxColor(220, 220, 220, 0xCC),
+		points1));
+	chartData.AddDataset(dataset1);
+
+	// Add the second dataset
+	wxVector<wxDouble> points2;
+	points2.push_back(1);
+	points2.push_back(1.33);
+	points2.push_back(2.5);
+	points2.push_back(2);
+	points2.push_back(3);
+	points2.push_back(1.8);
+	points2.push_back(0.4);
+	wxStackedColumnChartDataset::ptr dataset2(new wxStackedColumnChartDataset(
+		wxColor(151, 187, 205, 0x7F),
+		wxColor(151, 187, 205, 0xCC),
+		points2));
+	chartData.AddDataset(dataset2);
+
+	// Create the stacked bar chart widget
+	wxStackedColumnChartCtrl* stackedColumnChartCtrl = new wxStackedColumnChartCtrl(panel, wxID_ANY, chartData);
+
+	// Set up the sizer for the panel
+	wxBoxSizer* panelSizer = new wxBoxSizer(wxHORIZONTAL);
+	panelSizer->Add(stackedColumnChartCtrl, 1, wxEXPAND);
+	panel->SetSizer(panelSizer);
+
+	// Set up the sizer for the frame
+	wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
+	topSizer->Add(panel, 1, wxEXPAND);
+	SetSizerAndFit(topSizer);
+}
