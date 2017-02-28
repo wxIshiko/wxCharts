9469740a949b6d3c30e828eafbd81f7b904351d8
Kvaz1r <atomic.alarm@yandex.ru>
2017-02-28 10:26:35 +0200

    update example

--- samples/math2d/src/WxMath2DFrame.cpp
+++ samples/math2d/src/WxMath2DFrame.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2017 Xavier Leclercq
+    Copyright (c) 2015-2017 Xavier Leclercq
 
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
@@ -21,8 +21,60 @@
 */
 
 #include "WxMath2DFrame.h"
+#include <wx/panel.h>
+#include <wx/sizer.h>
+#include <wx/charts/wxcharts.h>
+#include <cmath>
 
 WxMath2DFrame::WxMath2DFrame(const wxString& title)
     : wxFrame(NULL, wxID_ANY, title)
 {
+    wxPanel* panel = new wxPanel(this, wxID_ANY);
+
+    wxMath2DPlotData chartData;
+    wxMath2DPlotOptions options;
+    options.SetShowTooltips(false);
+
+    wxVector<wxPoint2DDouble> points1,points2,points3;
+    auto pi = 3.1415926535897;
+    auto tstart = -2*pi;
+    for(auto i = 0u;i<100;i++)
+    {
+        auto x = tstart+0.1*i;
+        points1.push_back(wxPoint2DDouble(x,cos(x)*sin(x)));
+        points2.push_back(wxPoint2DDouble(x,cos(x)+sin(x)));
+        points3.push_back(wxPoint2DDouble(x,cos(x)-sin(x)));
+    }
+
+    wxMath2DPlotDataset::ptr dataset1(
+        new wxMath2DPlotDataset(
+            wxColor(250, 20, 20, 0x78),
+            wxColor(0, 0, 0, 0xB8),
+            points1,wxCHARTTYPE_STEM)
+        );
+    chartData.AddDataset(dataset1);
+
+     wxMath2DPlotDataset::ptr dataset2(
+        new wxMath2DPlotDataset(
+            wxColor(0, 70, 140, 0x78),
+            wxColor(50, 210, 105, 0xB8),
+            points2,wxCHARTTYPE_STEPPED)
+        );
+    chartData.AddDataset(dataset2);
+
+     wxMath2DPlotDataset::ptr dataset3(
+        new wxMath2DPlotDataset(
+            wxColor(28, 200, 10, 0x78),
+            wxColor(230, 125, 60, 0x33),points3));
+    chartData.AddDataset(dataset3);
+
+    wxMath2DPlotCtrl* math2dPlotCtrl = new wxMath2DPlotCtrl(panel, wxID_ANY, chartData,options);
+
+    wxBoxSizer* panelSizer = new wxBoxSizer(wxHORIZONTAL);
+    panelSizer->Add(math2dPlotCtrl, 1, wxEXPAND);
+    panel->SetSizer(panelSizer);
+
+    wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
+    topSizer->Add(panel, 1, wxEXPAND);
+    SetSizerAndFit(topSizer);
 }
