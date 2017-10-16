7c134ac55f3fc3900b4094f874d68560f9c03a25
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-10-16 10:04:58 +0100

    Changed tabs to spaces

--- include/wx/charts/wxbarchartdata.h
+++ include/wx/charts/wxbarchartdata.h
@@ -1,23 +1,23 @@
 /*
-	Copyright (c) 2016 Xavier Leclercq
+    Copyright (c) 2016-2017 Xavier Leclercq
 
-	Permission is hereby granted, free of charge, to any person obtaining a
-	copy of this software and associated documentation files (the "Software"),
-	to deal in the Software without restriction, including without limitation
-	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-	and/or sell copies of the Software, and to permit persons to whom the
-	Software is furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-	IN THE SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
 */
 
 /// @file
@@ -34,34 +34,34 @@
 class wxBarChartDataset
 {
 public:
-	/// Smart pointer typedef.
-	typedef wxSharedPtr<wxBarChartDataset> ptr;
+    /// Smart pointer typedef.
+    typedef wxSharedPtr<wxBarChartDataset> ptr;
 
-	/// Constructs a wxBarChartDataset instance.
-	/// @param fillColor The color of the brush used to 
-	/// fill the bar.
-	/// @param strokeColor The color of the pen used to
-	/// draw the outline of the bar.
-	/// @param data The list of values.
-	wxBarChartDataset(const wxColor &fillColor, const wxColor &strokeColor,
-		const wxVector<wxDouble> &data);
+    /// Constructs a wxBarChartDataset instance.
+    /// @param fillColor The color of the brush used to 
+    /// fill the bar.
+    /// @param strokeColor The color of the pen used to
+    /// draw the outline of the bar.
+    /// @param data The list of values.
+    wxBarChartDataset(const wxColor &fillColor, const wxColor &strokeColor,
+        const wxVector<wxDouble> &data);
 
-	/// Gets the color of the brush used to fill the
-	/// bar.
-	/// @return The color of the brush used to fill the
-	/// bar.
-	const wxColor& GetFillColor() const;
-	/// Gets the color of the pen used to draw the outline
-	/// of the bar.
-	/// @return The color of the pen used to draw the 
-	/// outline of the bar.
-	const wxColor& GetStrokeColor() const;
-	const wxVector<wxDouble>& GetData() const;
+    /// Gets the color of the brush used to fill the
+    /// bar.
+    /// @return The color of the brush used to fill the
+    /// bar.
+    const wxColor& GetFillColor() const;
+    /// Gets the color of the pen used to draw the outline
+    /// of the bar.
+    /// @return The color of the pen used to draw the 
+    /// outline of the bar.
+    const wxColor& GetStrokeColor() const;
+    const wxVector<wxDouble>& GetData() const;
 
 private:
-	wxColor m_fillColor;
-	wxColor m_strokeColor;
-	wxVector<wxDouble> m_data;
+    wxColor m_fillColor;
+    wxColor m_strokeColor;
+    wxVector<wxDouble> m_data;
 };
 
 /// Data for one of the bar chart controls.
@@ -70,23 +70,23 @@ private:
 class wxBarChartData
 {
 public:
-	/// Constructs a wxBarChartData instance.
-	/// @param labels The labels of the X axis.
-	wxBarChartData(const wxVector<wxString> &labels);
+    /// Constructs a wxBarChartData instance.
+    /// @param labels The labels of the X axis.
+    wxBarChartData(const wxVector<wxString> &labels);
 
-	/// Adds a dataset.
-	/// @param dataset The dataset to add.
-	void AddDataset(wxBarChartDataset::ptr dataset);
+    /// Adds a dataset.
+    /// @param dataset The dataset to add.
+    void AddDataset(wxBarChartDataset::ptr dataset);
 
-	/// Gets the labels of the X axis.
-	/// @return A vector containing the labels of the
-	/// X axis.
-	const wxVector<wxString>& GetLabels() const;
-	const wxVector<wxBarChartDataset::ptr>& GetDatasets() const;
+    /// Gets the labels of the X axis.
+    /// @return A vector containing the labels of the
+    /// X axis.
+    const wxVector<wxString>& GetLabels() const;
+    const wxVector<wxBarChartDataset::ptr>& GetDatasets() const;
 
 private:
-	wxVector<wxString> m_labels;
-	wxVector<wxBarChartDataset::ptr> m_datasets;
+    wxVector<wxString> m_labels;
+    wxVector<wxBarChartDataset::ptr> m_datasets;
 };
 
 #endif
