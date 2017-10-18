6c3c244d624ac5938c9adc24a70fab396d61c483
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-10-18 22:22:12 +0100

    Replaced tabs with spaces

--- include/wx/charts/wxpolarareachartoptions.h
+++ include/wx/charts/wxpolarareachartoptions.h
@@ -1,23 +1,23 @@
 /*
-	Copyright (c) 2016 Xavier Leclercq
+    Copyright (c) 2016-2017 Xavier Leclercq
 
-	Permission is hereby granted, free of charge, to any person obtaining a
-	copy of this software and associated documentation files (the "Software"),
-	to deal in the Software without restriction, including without limitation
-	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-	and/or sell copies of the Software, and to permit persons to whom the
-	Software is furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-	IN THE SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
 */
 
 /// @file
@@ -32,18 +32,18 @@
 class wxPolarAreaChartOptions : public wxChartOptions
 {
 public:
-	/// Constructs a wxPolarAreaChartOptions instance.
-	wxPolarAreaChartOptions();
+    /// Constructs a wxPolarAreaChartOptions instance.
+    wxPolarAreaChartOptions();
 
-	/// Gets the options for the grid.
-	/// @return The options for the grid.
-	const wxChartRadialGridOptions& GetGridOptions() const;
+    /// Gets the options for the grid.
+    /// @return The options for the grid.
+    const wxChartRadialGridOptions& GetGridOptions() const;
 
     wxDouble GetStartAngle() const;
     void SetStartAngle(wxDouble startAngle);
 
 private:
-	wxChartRadialGridOptions m_gridOptions;
+    wxChartRadialGridOptions m_gridOptions;
     wxDouble m_startAngle;
 };
 
