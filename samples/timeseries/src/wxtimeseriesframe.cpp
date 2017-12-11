895114c22b57dc739b8280841cf554ecf3b89e91
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-12-11 10:04:40 +0000

    Updates to time series tests

---
+++ samples/timeseries/src/wxtimeseriesframe.cpp
@@ -0,0 +1,47 @@
+/*
+	Copyright (c) 2017 Xavier Leclercq
+
+	Permission is hereby granted, free of charge, to any person obtaining a
+	copy of this software and associated documentation files (the "Software"),
+	to deal in the Software without restriction, including without limitation
+	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+	and/or sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+	IN THE SOFTWARE.
+*/
+
+#include "wxtimeseriesframe.h"
+#include <wx/panel.h>
+#include <wx/sizer.h>
+#include <wx/charts/wxcharts.h>
+
+WxTimeSeriesFrame::WxTimeSeriesFrame(const wxString& title)
+	: wxFrame(NULL, wxID_ANY, title)
+{
+	// Create a top-level panel to hold all the contents of the frame
+	wxPanel* panel = new wxPanel(this, wxID_ANY);
+
+	// Create the time series chart widget from the constructed data
+	wxTimeSeriesChartCtrl* timeSeriesChartCtrl = new wxTimeSeriesChartCtrl(panel, wxID_ANY,
+		wxDefaultPosition, wxDefaultSize, wxBORDER_NONE);
+
+	// Set up the sizer for the panel
+	wxBoxSizer* panelSizer = new wxBoxSizer(wxHORIZONTAL);
+	panelSizer->Add(timeSeriesChartCtrl, 1, wxEXPAND);
+	panel->SetSizer(panelSizer);
+
+	// Set up the sizer for the frame
+	wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
+	topSizer->Add(panel, 1, wxEXPAND);
+	SetSizerAndFit(topSizer);
+}
