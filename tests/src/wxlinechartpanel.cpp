5c326c8f885e0f7597bb9de0fb34af9bbabf62a2
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2019-01-12 19:31:33 +0000

    Update wxlinechartpanel.cpp

--- tests/src/wxlinechartpanel.cpp
+++ tests/src/wxlinechartpanel.cpp
@@ -21,8 +21,59 @@
 */
 
 #include "wxlinechartpanel.h"
+#include <wx/sizer.h>
 
 wxLineChartPanel::wxLineChartPanel(wxWindow* parent)
     : wxPanel(parent)
 {
+    // Create the data for the line chart widget
+    wxVector<wxString> labels;
+    labels.push_back("January");
+    labels.push_back("February");
+    labels.push_back("March");
+    labels.push_back("April");
+    labels.push_back("May");
+    labels.push_back("June");
+    labels.push_back("July");
+    wxLineChartData chartData(labels);
+
+    // Add the first dataset
+    wxVector<wxDouble> points1;
+    points1.push_back(3);
+    points1.push_back(-2.5);
+    points1.push_back(-1.2);
+    points1.push_back(3);
+    points1.push_back(6);
+    points1.push_back(5);
+    points1.push_back(1);
+    wxLineChartDataset::ptr dataset1(new wxLineChartDataset(
+        "My First Dataset", wxColor(220, 220, 220),
+        wxColor(255, 255, 255), wxColor(220, 220, 220, 0x33),
+        points1));
+    chartData.AddDataset(dataset1);
+
+    // Add the second dataset
+    wxVector<wxDouble> points2;
+    points2.push_back(1);
+    points2.push_back(-1.33);
+    points2.push_back(2.5);
+    points2.push_back(7);
+    points2.push_back(3);
+    points2.push_back(-1.8);
+    points2.push_back(0.4);
+    wxLineChartDataset::ptr dataset2(new wxLineChartDataset(
+        "My Second Dataset", wxColor(151, 187, 205),
+        wxColor(255, 255, 255), wxColor(151, 187, 205, 0x33),
+        points2));
+    chartData.AddDataset(dataset2);
+
+    // Create the line chart widget from the constructed data
+    m_lineChart = new wxLineChartCtrl(this, wxID_ANY, chartData,
+        wxDefaultPosition, wxDefaultSize, wxBORDER_NONE);
+
+    // Set up the sizer for the panel
+    wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
+    sizer->Add(m_lineChart, 1, wxEXPAND);
+
+    SetSizer(sizer);
 }
