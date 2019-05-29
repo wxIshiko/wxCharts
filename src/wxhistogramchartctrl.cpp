d1d681fb4c2640e0b5facef16a7d081aa3e732fe
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2019-05-29 22:42:07 +0100

    Rename files to be consistent with naming conventions

--- src/wxhistogramchartctrl.cpp
+++ src/wxhistogramchartctrl.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2018 Xavier Leclercq and the wxCharts contributors.
+    Copyright (c) 2018-2019 Xavier Leclercq and the wxCharts contributors.
 
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
@@ -33,7 +33,7 @@
 
 /// @file
 
-#include "wxhistchartctrl.h"
+#include "wxhistogramchartctrl.h"
 #include <wx/filedlg.h>
 
 wxHistogramChartCtrl::wxHistogramChartCtrl(wxWindow *parent,
