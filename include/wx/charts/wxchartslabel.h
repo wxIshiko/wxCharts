0493b5c9e43ef8ecd1677ead7506711258334564
abc <a@b.com>
2022-04-21 04:13:47 +0800

    put back wxChartsLabel::operator== and wxChartsLabel::operator!=

--- include/wx/charts/wxchartslabel.h
+++ include/wx/charts/wxchartslabel.h
@@ -60,6 +60,17 @@ public:
         wxDouble height, const wxChartsLabelOptions &options);
 
 public:
+    bool operator==(const wxChartsLabel & rhs)
+    {
+        return m_text == rhs.m_text;
+    }
+
+    bool operator!=(const wxChartsLabel & rhs)
+    {
+        return m_text != rhs.m_text;
+    }
+
+public:
     virtual void Draw(wxGraphicsContext &gc) const;
 
     virtual bool HitTest(const wxPoint &point) const;
