632f2771dfad9465022be8e1f2c76c98b4b516e5
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-06-04 13:13:54 +0100

    Fix header guard typo

--- include/wx/charts/wxareachartctrl.h
+++ include/wx/charts/wxareachartctrl.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2017-2019 Xavier Leclercq
+    Copyright (c) 2017-2022 Xavier Leclercq
 
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
@@ -34,7 +34,7 @@
 /// @file
 
 #ifndef _WX_CHARTS_WXAREACHARTCTRL_H_
-#define _WX_CHARTS_WXAREACHARTCTRL_H
+#define _WX_CHARTS_WXAREACHARTCTRL_H_
 
 #include "wxcharts_export.h"
 
