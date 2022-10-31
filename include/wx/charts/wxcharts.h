3a1aae0f02ede615abca8033320cfce0f66077cd
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-10-31 22:37:30 +0000

    Put back VS linking pragma

--- include/wx/charts/wxcharts.h
+++ include/wx/charts/wxcharts.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2015-2019 Xavier Leclercq and the wxCharts contributors.
+    Copyright (c) 2015-2022 Xavier Leclercq and the wxCharts contributors.
 
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
@@ -52,5 +52,15 @@
 #include "wxchartsthemeid.h"
 #include "wxchartstheme.h"
 #include "wxchartsthemefactory.h"
+
+#ifdef _MSC_VER
+
+#ifdef _DEBUG
+#pragma comment(lib, "wxchartsd.lib")
+#else
+#pragma comment(lib, "wxcharts.lib")
+#endif
+
+#endif
 
 #endif
